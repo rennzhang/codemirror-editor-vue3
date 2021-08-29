dd4d2ee7d38accb282f25a97bc8ef373c468631a
张韧 <906155099@qq.com>
2021-08-29 20:16:50 +0800

    test

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,7 +4,7 @@
 set -e
 
 # 生成静态文件
-npm run docs:build
+# npm run docs-build
 
 # 进入生成的文件夹
 cd docs/.vitepress/dist
@@ -14,12 +14,13 @@ cd docs/.vitepress/dist
 
 git init
 git add -A
-git commit -m 'deploy'
+git commit -m 'deploy docs'
+git branch -m master main
 
 # 如果发布到 https://<USERNAME>.github.io
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
-
+git push -f git@github.com:RennCheung/codemirror-editor-vue3.git docs
 cd -
