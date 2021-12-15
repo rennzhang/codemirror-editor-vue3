524b3aeb1204bb6b81dfa21e8d00310acb6429a6
张韧 <906155099@qq.com>
2021-12-15 14:18:18 +0800

    chore: *

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,2 +1,29 @@
-declare module "codemirror-editor-vue3";
-declare module "codemirror-editor-vue3-local";
+declare interface Fn<T = any, R = T> {
+  (...arg: T[]): R;
+}
+
+declare interface PromiseFn<T = any, R = T> {
+  (...arg: T[]): Promise<R>;
+}
+
+declare type RefType<T> = T | null;
+
+declare type LabelValueOptions = {
+  label: string;
+  value: any;
+  [key: string]: string | number | boolean;
+}[];
+
+declare type EmitType = (event: string, ...args: any[]) => void;
+
+declare type TargetContext = "_self" | "_blank";
+
+declare interface ComponentElRef<T extends HTMLElement = HTMLDivElement> {
+  $el: T;
+}
+
+declare type ComponentRef<T extends HTMLElement = HTMLDivElement> = ComponentElRef<T> | null;
+
+declare type ElRef<T extends HTMLElement = HTMLDivElement> = Nullable<T>;
+
+declare type Nullable<T> = T | null;
---
+++ types/module.d.ts
@@ -0,0 +1,20 @@
+declare module "*.vue" {
+  import { DefineComponent } from "vue";
+  const Component: DefineComponent<{}, {}, any>;
+  export default Component;
+}
+
+declare module "virtual:*" {
+  const result: any;
+  export default result;
+}
+
+declare module "*.less" {
+  const content: any;
+  export default content;
+}
+// 声明第三方库
+
+declare module "rollup-plugin-cpy";
+declare module "codemirror-editor-vue3";
+declare module "codemirror-editor-vue3-local";
