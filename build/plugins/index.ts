a19efd96e43cb4e462c3f4579f71a7f1829c2f6b
Renkin <906155099@qq.com>
2024-03-27 10:55:49 +0800

    chore: 去除 checker

--- build/plugins/index.ts
+++ build/plugins/index.ts
@@ -1,7 +1,6 @@
 import vue from "@vitejs/plugin-vue";
 import vueJsx from "@vitejs/plugin-vue-jsx";
 import AutoImport from "unplugin-auto-import/vite";
-import checker from "vite-plugin-checker";
 import dts from "vite-plugin-dts";
 import Pages from "vite-plugin-pages";
 import WindiCSS from "vite-plugin-windicss";
@@ -20,16 +19,6 @@ export default [
     },
     // Generate corresponding .eslintrc-auto-import.json file.
     // eslint globals Docs - https://eslint.org/docs/user-guide/configuring/language-options#specifying-globals
-  }),
-  checker({
-    typescript: true,
-    vueTsc: true,
-    eslint: {
-      lintCommand: 'eslint "./packages/**/*.{ts,tsx,vue}"',
-      dev: {
-        logLevel: ["error"],
-      },
-    },
   }),
   dts(),
   libInjectCss(),
