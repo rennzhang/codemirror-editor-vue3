5636c6f783778ab51deb7b3b9c671d8f93b65eeb
zhangren <zhangren.work@qq.com>
2025-10-06 17:29:29 +0800

    feat: 更新文档部署脚本

--- scripts/deploy-docs.sh
+++ scripts/deploy-docs.sh
@@ -15,7 +15,6 @@ rm -rf .git
 git init
 git add -A
 git commit -m 'deploy docs'
-git branch -m master main
 
 # 如果发布到 https://<USERNAME>.github.io
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
